11bafdca4b9d47ef5d36060c9c4797dc51160185
Donghyun Lim <ehdgus4173@gmail.com>
2026-04-01 20:22:14 +0900

    Feat: add contents at 'home' and apply responsive design for navigation bar & home contents

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Add active class to current page link
     const links = document.querySelectorAll('.nav-bar a');
+
     links.forEach(link => {
         if (link.href === location.href) {
             link.classList.add('active');
         }
     });
+
+    // Hamburger button
+    const hamburgerBtn = document.getElementById('hamburger-btn');
+    const navMenu = document.querySelector('.nav-bar ul');
+
+    hamburgerBtn.addEventListener('click', () => {
+        navMenu.classList.toggle('open');
+    });
+
+
+
+
 });
